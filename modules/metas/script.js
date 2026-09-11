61820c0d6752ba91729ea5c8639d805303b35978
unknown <pedrorochabispo@gmail.com>
2026-09-11 15:35:35 -0300

    feat: mudança de estrutura

---
+++ modules/metas/script.js
@@ -0,0 +1,4 @@
+﻿// Inicializador do módulo.
+// Este arquivo coordena a execução das funções exportadas dos arquivos em /js,
+// usando import/export e async functions para montar a lógica principal do módulo.
+.
