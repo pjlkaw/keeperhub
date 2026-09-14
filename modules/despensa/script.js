b327d879403947bddd065334a30a59e44232e665
Sergio <sergiorafaelgithub520@gmail.com>
2026-09-13 22:46:24 -0300

    docs

--- modules/despensa/script.js
+++
@@ -1,4 +0,0 @@
-﻿// Inicializador do módulo.
-// Este arquivo coordena a execução das funções exportadas dos arquivos em /js,
-// usando import/export e async functions para montar a lógica principal do módulo.
-.
