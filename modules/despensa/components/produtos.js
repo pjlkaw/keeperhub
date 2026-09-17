bd376cefbe093ae0e5647b45c0c3714a1a0aa301
estephany <estephanycamilly.92@gmail.com>
2026-09-17 19:13:05 -0300

    feat: atualizacao index e css

--- modules/despensa/components/produtos.js
+++ modules/despensa/components/produtos.js
@@ -205,3 +205,16 @@ let filtroAtual = "Todos";
     function voltar() {
       window.history.back();
     }
+
+  
+    fetch('/shared/components/navbar-section.html')
+        .then(response => response.text())
+        .then(data => {
+            const nav = document.getElementById('modules-nav-wrapper');
+            nav.innerHTML = data;
+
+            const moduloAtual = nav.querySelector('[data-module="despensa"]');
+            moduloAtual.classList.add('active');
+            moduloAtual.setAttribute('aria-selected', 'true');
+            moduloAtual.setAttribute('tabindex', '0');
+        });
